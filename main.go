f4809ee6aa8a1ad46450cf2a833509d4dac7ad1c
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-04-15 15:40:53 +0100

    data type

--- main.go
+++ main.go
@@ -6,17 +6,27 @@ import "fmt"
 
 
 func main(){
-	var conferenceName="Go Conference ";
-	const conferenceTickets=50;
-	var remainingTickets=50;
-
-
-	fmt.Println("Welcome to",conferenceName, "booking application");
-	fmt.Println("we have a total of ",conferenceTickets," tickets and  remaining",remainingTickets,)
-	fmt.Println("Get your ticket here to attend ");
+	conferenceName :="Go Conference ";
+	const conferenceTickets int=50;
+	var remainingTickets uint=50;
 	
 
+	fmt.Printf("conferenceName is %T conferenceTickets is %T  remainingTickets is a %T",conferenceName,conferenceTickets,remainingTickets);
 
-	fmt.Println(conferenceName);
+
+	fmt.Printf("Welcome to %v booking application\n",conferenceName);
+	fmt.Printf("we have a total of %v  tickets and  remaining %v",remainingTickets,conferenceTickets)
+	fmt.Println("Get your ticket here to attend ");
+	
+var userName string
+
+var userTickets int
+
+
+userName="chuks";
+userTickets=2
+fmt.Println(userName)
+fmt.Printf("usern %v booked %v tickets",userName,userTickets,)
+
 
 }
